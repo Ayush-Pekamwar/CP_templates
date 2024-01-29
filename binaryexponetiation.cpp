d8d4f56c155cf2ea421037645da088837f273cbc
ayush-pekamwar <120619186+Ayush-Pekamwar@users.noreply.github.com>
2024-01-29 08:05:36 +0530

    Create binaryexponetiation.cpp

---
+++ binaryexponetiation.cpp
@@ -0,0 +1,14 @@
+long long binpow(long long a, long long b)
+{
+    //to compute a^b
+
+    long long res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
