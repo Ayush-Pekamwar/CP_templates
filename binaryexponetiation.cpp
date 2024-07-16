89ee466f25f974ddc2fdf80f84b07f3726b6dd6d
ayush-pekamwar <120619186+Ayush-Pekamwar@users.noreply.github.com>
2024-07-16 13:09:58 +0530

    Update binaryexponetiation.cpp

--- binaryexponetiation.cpp
+++ binaryexponetiation.cpp
@@ -1,14 +1,13 @@
-long long binpow(long long a, long long b)
-{
-    //to compute a^b
-
-    long long res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
+const int M  = 1e9+7;
+long long binpow(long long a, long long b) {
+        // calculates a^b % M
+        long long res = 1;
+        a%=M;
+        while (b > 0) {
+            if (b & 1)
+                res = (res * a)% M;
+            a = (a * a)%M;
+            b = b>>1;
+        }
+        return res;
     }
-    return res;
-}
